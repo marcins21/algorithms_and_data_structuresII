5508d03b7903a1a3d96f01f131f73c760ab0d95e
idontfeelsogoood <msitko21@interia.pl>
2023-02-27 16:01:34 +0100

    array done

---
+++ set_implementation/setList.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include <vector>
+
+class setList
+{
+    std::vector<int> vec;
+
+public:
+
+    int getSize();  
+    void printSet(); 
+    void insert(int x);
+    void withdraw(int x);
+    bool isInSet(int x); 
+    setList operator+(setList& obj);  
+    setList operator*(setList& obj);   
+    setList operator-(setList& obj);
+    bool operator==(setList& obj);  
+    bool operator<=(setList& obj);
+    
+};
+
+
---
+++ set_implementation/set_arr.cpp
@@ -0,0 +1,80 @@
+#include "setArr.h"
+
+
+setArr::setArr(){
+    size=100;
+    table=new bool[size];
+    
+    /* Ustawienie wszystkich komorek na False*/
+    for(int i=0; i < size; i++){
+        table[i]=false;
+    }
+}
+
+int setArr::getSize(){
+    return size;
+}
+
+void setArr::printSet(){
+    int array_size = setArr::getSize();
+    for(int i=0; i < size; i++){
+        if (table[i] == true){
+            std::cout<<i<<" ";
+        }
+    }
+}
+
+void setArr::insert(int x){
+    bool is_x_exists = setArr::isInSet(x);
+    bool correctNumber = setArr::checkRangeCorrectness(x);
+    if (correctNumber==true){
+        if (is_x_exists == false){
+            table[x]=true;
+        }else{
+            std::cout<<"\nLiczba "<<x<<" Juz istnieje w zbiorze"<<std::endl;
+        }
+    }else{
+        std::cout<<"\nPodana liczba jest spoza przedziału 0-100"<<std::endl;
+    }  
+}
+
+void setArr::withdraw(int i){
+    bool correctNumber = setArr::checkRangeCorrectness(i);
+    bool is_i_exists = setArr::isInSet(i);
+    if (correctNumber == true){
+        std::cout<<"\nPodano zly numer przyjmuje numery od -99 do +99"<<std::endl;
+    }
+    if (is_i_exists == '1'){
+        table[i]=false;
+    }
+    else{
+        std::cout<<"\nPodana liczba nie istnieje "<<std::endl;
+    }
+
+}
+bool setArr::isInSet(int i){
+    if (table[i] == true){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool setArr::checkRangeCorrectness(int x){
+    if ((x < 100) && (x > 0)){
+        return true;
+    }else{
+        return false;
+    }
+}
+int main(){
+    /* SetArray Obejct*/
+    setArr set_array;
+    set_array.insert(9);
+    set_array.printSet();    
+   
+    return 0;
+
+
+}
+
+
