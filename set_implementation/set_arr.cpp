a88871f000c70e63e8fcb6a6a0711967a8ccdbdf
idontfeelsogoood <msitko21@interia.pl>
2023-03-03 17:50:56 +0100

    Operators '-','*','<=','==' DONE

--- set_implementation/set_arr.cpp
+++ set_implementation/set_arr.cpp
@@ -7,7 +7,10 @@ setArr::setArr(){
     size=100;
     table=new bool[size];
     
-    /* Ustawienie wszystkich komorek na False*/
+    /* 
+        Ustawienie wszystkich komorek na False
+        Niestety u mnie kompilator domsylnie nie ustawia wszystkich boolean'ow na false
+    */
     for(int i=0; i < size; i++){
         table[i]=false; 
     }
@@ -15,6 +18,15 @@ setArr::setArr(){
 
 int setArr::getSize(){
     return size;
+}
+
+void setArr::clearSet(){
+
+    /* Ustawienie wszystkich komorek na False*/
+    for(int i=0; i < size; i++){
+        table[i]=false; 
+    }
+
 }
 
 void setArr::printSet(){
@@ -35,7 +47,8 @@ void setArr::insert(int x){
         }else{
             std::cout<<"\nLiczba "<<x<<" Juz istnieje w zbiorze"<<std::endl;
         }
-    }else{
+    }
+    else{
         std::cout<<"\nPodana liczba jest spoza przedzialu 0-100"<<x<<std::endl;
     }  
 }
@@ -57,58 +70,129 @@ void setArr::withdraw(int i){
 bool setArr::isInSet(int i){
     if (table[i] == true){
         return true;
-    }else{
+    }
+    else{
         return false;
     }
 }
 bool setArr::checkRangeCorrectness(int x){
-    if ((x < 100) && (x > 0)){
+    if ((x < setArr::universeSize) && (x > 0)){
         return true;
-    }else{
+    }
+    else{
         return false;
     }
 }
 
-/* Operator Dodawania*/
+
+// Operator dodawania zbiorów
 setArr setArr::operator+(setArr& object){
-    for(int i=0; i < 100; i ++){
-        if((object.table[i] == true)){
-            object.table[i]==true;
-        }
-        if(setArr::isInSet(i)==true){
-            object.table[i]=true;        
+    setArr result; 
+
+    for(int i=0; i<universeSize; i++){ 
+        if(table[i] || object.isInSet(i)){ 
+            result.insert(i); 
         }
     }
-    return object;
 
+    return result; 
+}
+
+// Operator iloczynu zbiorów
+setArr setArr::operator*(setArr& object){
+    setArr result; 
+
+    for(int i=0; i<universeSize; i++){ 
+        if(table[i] && object.isInSet(i)){ 
+            result.insert(i); 
+        }
     }
 
+    return result; 
+}
+
+
+
+// Operator różnicy zbiorów
+setArr setArr::operator-(setArr& object){
+    setArr result;
+
+    for(int i=0; i<universeSize; i++){ 
+        if(table[i] && !object.isInSet(i)){ 
+            result.insert(i); 
+        }
+    }
+
+    return result; 
+}
+
+// Operator rownosci zbiorow
+bool setArr::operator==(setArr& object){
+    for(int i=0; i<universeSize; i++){
+        if(table[i] != object.isInSet(i)){ 
+            return false; 
+        }
+    }
+
+    return true; 
+}
+
+//Operator podzbioru
+bool setArr::operator<=(setArr& object){
+    for(int i=0; i<universeSize; i++){ 
+        if(table[i] && !object.isInSet(i)){ 
+            return false; 
+        }
+    }
+
+    return true; 
+}
 
 
 
 int main(){
-    /* SetArray Object*/
+  /* SetArray Object*/
     setArr set_array;
     setArr set_array1;
 
     set_array.insert(9);
-    set_array.insert(8);
+    set_array.insert(7);
     set_array.insert(10);
-
+    set_array.insert(20);
+    set_array.insert(21);
     set_array.insert(19);
 
     set_array.withdraw(19);
+    std::cout << "Array1:" << std::endl;
     set_array.printSet();   
 
     set_array1.insert(9);
     set_array1.insert(10);
 
-    std::cout<<" "<<std::endl;
-    setArr set_array3 = set_array+set_array1;
-    set_array3.printSet();
-   
-    return 0;
+    std::cout << "\nArray2:" << std::endl;
+    set_array1.printSet();
 
+    setArr set_array3 = set_array + set_array1; // dodajemy zbiory
+    setArr set_array4 = set_array - set_array1; // różnica zbiorów
+
+    std::cout << "\nRoznica:" << std::endl;
+    set_array4.printSet();
+
+    if(set_array == set_array1) {
+        std::cout << "\nZbiory sa rowne." << std::endl;
+    } 
+    else {
+        std::cout << "\nZbiory sa rozne." << std::endl;
+    }
+
+    if(set_array <= set_array3) {
+        std::cout << "\nArray1 jest podzbiorem Array3 --> Array1+Array2." << std::endl;
+    } 
+    else {
+        std::cout << "\nArray1 nie jest podzbiorem Array3 --> Array1+Array2." << std::endl;
+    }
+
+return 0;
 
 }
 
