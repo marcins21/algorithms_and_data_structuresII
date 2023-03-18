ee491937a8472cab04a80e63633418db6b2f1be3
idontfeelsogoood <msitko21@interia.pl>
2023-03-18 19:20:31 +0100

    priority queue done

--- priority_queue/priority_queue.cpp
+++ priority_queue/priority_queue.cpp
@@ -16,29 +16,48 @@ int rightChild(int i){
     return ((2*i)+2);
 }
 
-// Shifting node, and maintaing heap structure
 void maintain_heap_structure(int i){
     while (i > 0 && H[parent(i)] < H[i]){
         swap(H[parent(i)],H[i]);
         i=parent(i);
     }
 }
-
 void KolejkaPriorytetowa::insert(int x){
     size = size+1;
     H[size] = x;
     maintain_heap_structure(size);
 }
-
 int KolejkaPriorytetowa::getRootElem(){
     return H[0];
 }
-
 int KolejkaPriorytetowa::RemoveRootElem(){
-    //TODO
-    return 0;
-}
+    if(size < 0){ 
+        return -1; 
+    }
+    int root = H[0];
+    H[0] = H[size];
+    size = size-1;
 
+    int i = 0;
+    while (i <= size){
+        int max = i;
+        if (leftChild(i) <= size && H[leftChild(i)] > H[max]){
+            max = leftChild(i);
+        }
+        if (rightChild(i) <= size && H[rightChild(i)] > H[max]){
+            max = rightChild(i);
+        }
+        if (max != i){
+            swap(H[i], H[max]);
+            i = max;
+        }
+        else{ 
+            break;
+        }
+    }
+
+    return root;
+}
 void KolejkaPriorytetowa::print(){
     for (int i=0; i <= size ; i++){
         std::cout<<H[i]<<std::endl;
@@ -47,19 +66,36 @@ void KolejkaPriorytetowa::print(){
         }
     }
 }
-
 int main(){
 
     KolejkaPriorytetowa q1;
+
     q1.insert(45);
     q1.insert(10);
     q1.insert(14);
     q1.insert(12);
+
+    std::cout << "Kolejka po wstawieniu elementow: " << std::endl;
     q1.print();
 
     int root = q1.getRootElem();
-    std::cout<<"root element"<<root<<std::endl;
-    
-    std::cout<<"\nHello World!\n";
+    std::cout<<"Wartosc korzenia: "<<root<<std::endl;
+
+    int removed = q1.RemoveRootElem();
+    std::cout<<"Usuniety element: "<<removed<<std::endl;
+
+    std::cout << "Kolejka po usunieciu korzenia: " << std::endl;
+    q1.print();
+
+    root = q1.getRootElem();
+    std::cout<<"Nowa wartosc korzenia: "<<root<<std::endl;
+
+
+    std::cout<<std::endl;
+    q1.print();
     return 0;
 }
+
+
+
+
