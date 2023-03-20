a0971ad2d7d5e089b6c1d68cc4be0081e3b7031f
idontfeelsogoood <msitko21@interia.pl>
2023-03-20 15:21:41 +0100

    Adding BFS and DFS

---
+++ graph_search/bfs.py
@@ -0,0 +1,69 @@
+from collections import defaultdict
+
+class Graph:
+    # Constructor
+    def __init__(self, num_of_nodes):
+        self.m_num_of_nodes = num_of_nodes
+        self.edges = defaultdict(list)
+	
+    def addEdge(self, node1, node2):        
+        self.edges[node1].append(node2)
+        
+    def print_edge_list(self):
+        for k,v in self.edges.items():
+            print("edge ", k, ": ", v)
+
+    def check_neighboors(self,vertex):
+        if self.edges[vertex]:
+            return True
+        else:
+            return False
+
+    def get_neighboors(self,vertex):
+        neighboors = []
+        for edge in self.edges[vertex]:
+            neighboors.append(edge)
+        return neighboors
+
+    def bfs_search(self,s):
+        visited = [False] * (self.m_num_of_nodes+1)
+        queue = []
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+            for i in self.edges[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+            
+def main():
+    #Wczytanie pliku grapf.txt
+    edges = []
+    with open("graph_search\graf.txt","r") as file:
+        read_content = file.readlines()
+        amound_of_lines = int(read_content[0])
+        for i in range(1,amound_of_lines):
+            edge = read_content[i].split()
+            edges.append([int(edge[0]),int(edge[1])])
+    
+    g = Graph(amound_of_lines)
+
+    #doddwanie krawedzi
+    for ed in edges:
+        g.addEdge(ed[0],ed[1])
+
+    print(f"\nbfs dla 1 ")
+    g.bfs_search(1)
+    print(f"\nbfs dla 2 ")
+    g.bfs_search(2)
+    print(f"\nbfs dla 4 ")
+    g.bfs_search(4)
+    print(f"\nbfs dla 7 ")
+    g.bfs_search(7)
+    print(f"\nbfs dla 11")
+    g.bfs_search(11)
+ 
+main()
---
+++ graph_search/dfs.py
@@ -0,0 +1,68 @@
+from collections import defaultdict
+
+class Graph:
+    # Constructor
+    def __init__(self, num_of_nodes):
+        self.m_num_of_nodes = num_of_nodes
+        self.edges = defaultdict(list)
+	
+    def addEdge(self, node1, node2):        
+        self.edges[node1].append(node2)
+        
+    def print_edge_list(self):
+        for k,v in self.edges.items():
+            print("edge ", k, ": ", v)
+
+    def check_neighboors(self,vertex):
+        if self.edges[vertex]:
+            return True
+        else:
+            return False
+
+    def get_neighboors(self,vertex):
+        neighboors = []
+        for edge in self.edges[vertex]:
+            neighboors.append(edge)
+        return neighboors
+    
+    def DFSUtil(self, v, visited):
+        visited.add(v)
+        print(v, end=' ')
+        for neighbour in self.edges[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    def DFS(self, v):
+        visited = set()
+        self.DFSUtil(v, visited)
+
+           
+def main():
+    #Wczytanie pliku grapf.txt
+    edges = []
+    with open("graph_search\graf.txt","r") as file:
+        read_content = file.readlines()
+        amound_of_lines = int(read_content[0])
+        for i in range(1,amound_of_lines):
+            edge = read_content[i].split()
+            edges.append([int(edge[0]),int(edge[1])])
+    
+    g = Graph(amound_of_lines)
+
+    #doddwanie krawedzi
+    for ed in edges:
+        g.addEdge(ed[0],ed[1])
+
+    print(f"\nAlgorytm DFS dla 1 ")    
+    g.DFS(1)
+    print(f"\nAlgorytm DFS dla 2 ")   
+    g.DFS(2)
+    print(f"\nAlgorytm DFS dla 4 ")   
+    g.DFS(4)
+    print(f"\nAlgorytm DFS dla 7 ")   
+    g.DFS(7)
+    print(f"\nAlgorytm DFS dla 11 ")   
+    g.DFS(11)
+
+ 
+main()
