892f840c6b1d71e0596a7506975bc0de4b876f68
idontfeelsogoood <msitko21@interia.pl>
2023-04-23 21:07:08 +0200

    topological sort done

--- kolokwium1/graph_kol.py
+++ kolokwium1/graph_kol.py
@@ -8,7 +8,7 @@ class Graph:
         self.number_of_vertecies = number_of_vertecies
         self.list_of_edges = defaultdict(list)
         self.list_of_wages = defaultdict(list)
-        self.list_of_all_vertecies = []
+        self.list_of_all_vertecies = set()
         self.visited = []
 
     def check_neighbour(self,node):
@@ -25,8 +25,8 @@ class Graph:
         self.list_of_wages[wage].append([node1,node2])
 
         #Tracking all vertecies
-        self.list_of_all_vertecies.append(node1)
-        self.list_of_all_vertecies.append(node2)
+        self.list_of_all_vertecies.add(node1)
+        self.list_of_all_vertecies.add(node2)
 
     def delete_all_edges_from_vertex(self,vertex):
         if vertex in self.list_of_edges.keys():
@@ -53,8 +53,53 @@ class Graph:
 
     def get_number_of_vertecies(self):
         return self.number_of_vertecies
+    
+    def amount_of_edges_goin_in_vertex(self):
+        counter = 0
+        dicionary_of_in_links = {}
+        queue =  []
 
+        for item in self.list_of_all_vertecies:
+            dicionary_of_in_links.setdefault(item,0)
+            queue.append(item)
+        
+        #XD! Brute-forcing algorithm XD!
+        for i in range(len(queue)):
+            checking = queue[i]
+            for k,v in self.list_of_edges.items():
+                for key in v:
+                    for keys in key:
+                        if keys == checking:
+                            dicionary_of_in_links[checking] += 1
+            counter +=1
+        return dicionary_of_in_links
+    
 
+    def topological_sort(self,links):
+        queue = []
+        result = []
+        
+        for k,v in links.items():
+            #print(k,v)
+            if v == 0:
+                queue.append(k)
+       
+        while queue:
+            a = queue.pop(0)
+            result.append(a)
+
+            for neighbour in self.list_of_edges[a]:
+                for k in neighbour.keys():
+                    if links[k] != 0:
+                        links[k] -= 1
+
+                for j in neighbour.keys():
+                    if links[j] == 0: 
+                        queue.append(j)
+        print(result)
+                  
+
+        
     def BFS(self,starting):
         queue = []
         queue.append(starting)
@@ -78,16 +123,11 @@ class Graph:
                     visited.add(k)
                     self.DFS_util(visited,k)
 
-        
-
     def DFS(self,starting):
         vistied  = set()
         self.DFS_util(vistied,starting)
               
 
-
-
-  
 def reading_graph_from_file(path):
     counter = 0
     with open(path,"r") as file1:
@@ -108,14 +148,18 @@ def reading_graph_from_file(path):
     return graph
     
 
-
-
 def main():
     g = reading_graph_from_file("kolokwium1/graf.txt")
-    g.print_edges()
+    #g.print_edges()
+
+    print("BFS")
     #g.BFS(1)
-    g.DFS(1)
-  
+    print("\nDFS")
+    #g.DFS(1)
+
+    print()
+    links = g.amount_of_edges_goin_in_vertex()
+    g.topological_sort(links)
     
     
 main()
